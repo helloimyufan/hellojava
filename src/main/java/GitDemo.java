393ab6ab3bcc5c125189e79e6522d6d22504ba06
helloimyufan <812614544@qq.com>
2018-09-13 15:00:51 +0800

    qwer

---
+++ src/main/java/GitDemo.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName GitDemo
+ * @Description TODO
+ * @Auther hahaha
+ * @Date 2018/9/13/013 14:58
+ * @Version 1.0
+ **/
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("haha");
+
+    }
+}
